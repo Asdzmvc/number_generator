0eb81cdbdf658c6f0a6d568dc076a8b3acff560f
Asdzmvc <1yulchievasadbek0071@gmail.com>
2024-03-28 11:41:58 +0500

    ...

--- app.js
+++ app.js
@@ -13,18 +13,28 @@ function generate(passwordLength, includeLowercase, includeUppercase, includeNum
     allowedChars += includeNumbers ? numberChars : "";
     allowedChars += includeSymbols ? symbolChars : "";
 
+    console.log(allowedChars);
+
 
 
     if (length <= 0){
-        return `(password length must be at least one!)`;
+        return `(password length must be at least 1!)`;
+    }
+    if (allowedChars.length === 0){
+        return `At least 1 set of chars needs to be selected!`;
+    }
+
+    for(let i = 0; i < length; i++){
+        const randomIndex = Math.floor(Math.random() * allowedChars.length);
+        password += allowedChars[randomIndex];
     }
 
     return '';
 }
 
 const passwordLength = 0;
-const includeLowercase = true;
-const includeUppercase = true;
+const includeLowercase = false;
+const includeUppercase = false;
 const includeNumbers = true;
 const includeSymbols = true;
 
@@ -34,4 +44,9 @@ const password = generate(passwordLength,
                           includeNumbers,
                           includeSymbols);
 
-console.log(`Generated password ${password}`);                          
+// console.log(`Generated password ${password}`);    
+
+
+
+
+// TO BE CONTINUED
